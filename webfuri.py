27b4d3378af0118a0cf2bf355e8813cf0c212a7f
KunnAOV06 <lebanam8.4.2006@gmail.com>
2026-06-10 14:21:43 +0700

    Update webfuri.py

--- webfuri.py
+++ webfuri.py
@@ -18,7 +18,7 @@ except ImportError:
     PIL_AVAILABLE = False
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key-change-this-in-production'
+app.config['SECRET_KEY'] = 'furi-web-furiedit'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datafuri.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'uploads'
